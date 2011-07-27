ca09c87af6633c4032f38875e3464ded95b9574f
Wicla <wicla@thethundertemplars.com>
2011-07-27 11:12:00 +0200

    Added all common changes to client/iptables-client.sh aswell.
    Mostly variable-names.

--- client/iptables-client.sh
+++ client/iptables-client.sh
@@ -36,15 +36,15 @@ USEWICD=false
 
 # NICs. Only LANIF is needed to be configured if WLAN isn't used.
 # Configure interfaces correctly. It's from this value it gets the source/destination IP- and broadcast-address to allow.
-LANIF=eth0
-WLANIF=wlan0
+HOST_LANIF=eth0
+HOST_WLANIF=wlan0
 
 # Dynamically opened ports. Whitespace separates each value. Both port number and service name allowed (see /etc/services)
-# IN_TCP_PORTS, IN_UDP_PORTS, OUT_TCP_PORTS and OUT_UDP_PORTS are arrays. Please enter each port number (or service name) seperated by space.
-IN_TCP_PORTS=( ) 
-IN_UDP_PORTS=( )
-OUT_TCP_PORTS=( )
-OUT_UDP_PORTS=( )
+# PORTS_TCPIN, PORTS_UDPIN, PORTS_TCPOUT and PORTS_UDPOUT are arrays. Please enter each port number (or service name) seperated by space.
+PORTS_TCPIN=( )
+PORTS_UDPIN=( )
+PORTS_TCPOUT=( )
+PORTS_UDPOUT=( )
 
 # Used for reject (if no INPUT connection is matched). Reject first 10 packets each 10 minute, then just drop them. 
 LIMIT="-m hashlimit --hashlimit 10/minute --hashlimit-burst 10 --hashlimit-mode srcip --hashlimit-name limreject"
@@ -53,9 +53,9 @@ LIMIT="-m hashlimit --hashlimit 10/minute --hashlimit-burst 10 --hashlimit-mode 
 # Variable $IF contains your NIC. Please make sure this value is correct. 
 if [ $USEWICD == true ]; then
   if [ "$1" == 'wireless' ]; then
-    IF=$WLANIF
+    HOST_IF=$HOST_WLANIF
   elif [ "$1" == 'wired' ]; then
-    IF=$LANIF
+    HOST_IF=$HOST_LANIF
   else
     echo 'Something is wrong.'
     echo 'Missing argument (which should be wireless or wired)'
@@ -63,13 +63,13 @@ if [ $USEWICD == true ]; then
     exit 1
   fi
 else
-  IF=$LANIF
+  HOST_IF=$HOST_LANIF
 fi
 
 # IP contains the IP-address of interface $IF
 # BCAST contains the broadcast address of interface $IF
-IP=($(ifconfig $IF)); IP=$(echo ${IP[6]#*:})
-BCAST=($(ifconfig $IF)); BCAST=$(echo ${IP[7]#*:})
+HOST_IP=($(ifconfig $HOST_IF)); HOST_IP=$(echo ${HOST_IP[6]#*:})
+HOST_BCAST=($(ifconfig $HOST_IF)); HOST_BCAST=$(echo ${HOST_BCAST[7]#*:})
 
 # LOGGING defines if unmatched connections should be logged.
 # Possibles values are following:
@@ -81,8 +81,8 @@ if [ $LOGGING = true \
   -o $(echo $LOGGING | tr [:lower:] [:upper:]) = INPUT \
   -o $(echo $LOGGING | tr [:lower:] [:upper:]) = OUTPUT ]; then
 # Chain for INPUT and OUTPUT for LOGGING
-  LOGINPUTCHAIN=LOGINPUT
-  LOGOUTPUTCHAIN=LOGOUTPUT
+  CHAIN_LOGINPUT=LOGINPUT
+  CHAIN_LOGOUTPUT=LOGOUTPUT
 # Loglimit and loglimitburst
   LOGLIMIT='1/min'
   LOGLIMITBURST='1'
@@ -114,18 +114,18 @@ $IPTABLES -P FORWARD DROP
 
 # If LOGGING is true or matches 'INPUT' - INPUT connections are passed through this chain
 if [ $LOGGING = true -o $(echo $LOGGING | tr [:lower:] [:upper:]) = INPUT ]; then
-  $IPTABLES -N $LOGINPUTCHAIN
-  $IPTABLES -A $LOGINPUTCHAIN -p tcp -m limit --limit $LOGLIMIT --limit-burst $LOGLIMITBURST -j LOG --log-prefix 'INPUT TCP: '
-  $IPTABLES -A $LOGINPUTCHAIN -p udp -m limit --limit $LOGLIMIT --limit-burst $LOGLIMITBURST -j LOG --log-prefix 'INPUT UDP: '
-  $IPTABLES -A $LOGINPUTCHAIN -p icmp -m limit --limit $LOGLIMIT --limit-burst $LOGLIMITBURST -j LOG --log-prefix 'INPUT ICMP: '
+  $IPTABLES -N $CHAIN_LOGINPUT
+  $IPTABLES -A $CHAIN_LOGINPUT -p tcp -m limit --limit $LOGLIMIT --limit-burst $LOGLIMITBURST -j LOG --log-prefix 'INPUT TCP: '
+  $IPTABLES -A $CHAIN_LOGINPUT -p udp -m limit --limit $LOGLIMIT --limit-burst $LOGLIMITBURST -j LOG --log-prefix 'INPUT UDP: '
+  $IPTABLES -A $CHAIN_LOGINPUT -p icmp -m limit --limit $LOGLIMIT --limit-burst $LOGLIMITBURST -j LOG --log-prefix 'INPUT ICMP: '
 fi
 
 # If LOGGING is true or matches 'OUTPUT' - OUTPUT connections are passed through this chain
 if [ $LOGGING = true -o $(echo $LOGGING | tr [:lower:] [:upper:]) = OUTPUT ]; then
-  $IPTABLES -N $LOGOUTPUTCHAIN
-  $IPTABLES -A $LOGOUTPUTCHAIN -p tcp -m limit --limit $LOGLIMIT --limit-burst $LOGLIMITBURST -j LOG --log-prefix 'OUTPUT TCP: '
-  $IPTABLES -A $LOGOUTPUTCHAIN -p udp -m limit --limit $LOGLIMIT --limit-burst $LOGLIMITBURST -j LOG --log-prefix 'OUTPUT UDP: '
-  $IPTABLES -A $LOGOUTPUTCHAIN -p icmp -m limit --limit $LOGLIMIT --limit-burst $LOGLIMITBURST -j LOG --log-prefix 'OUTPUT ICMP: '
+  $IPTABLES -N $CHAIN_LOGOUTPUT
+  $IPTABLES -A $CHAIN_LOGOUTPUT -p tcp -m limit --limit $LOGLIMIT --limit-burst $LOGLIMITBURST -j LOG --log-prefix 'OUTPUT TCP: '
+  $IPTABLES -A $CHAIN_LOGOUTPUT -p udp -m limit --limit $LOGLIMIT --limit-burst $LOGLIMITBURST -j LOG --log-prefix 'OUTPUT UDP: '
+  $IPTABLES -A $CHAIN_LOGOUTPUT -p icmp -m limit --limit $LOGLIMIT --limit-burst $LOGLIMITBURST -j LOG --log-prefix 'OUTPUT ICMP: '
 fi
 
 ####################### IPTABLES INPUT RULES ###################################
@@ -136,30 +136,30 @@ $IPTABLES -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 # Accept connections from loopback
 $IPTABLES -A INPUT -i lo -j ACCEPT
 
-# Accept all custom TCP-ports defined by $IN_TCP_PORTS
-for TCPPORT in ${IN_TCP_PORTS[@]}; do
-  $IPTABLES -A INPUT -p tcp --dport $TCPPORT -d $IP -j ACCEPT
+# Accept all custom TCP-ports defined by $PORTS_TCPIN
+for TCPPORT in ${PORTS_TCPIN[@]}; do
+  $IPTABLES -A INPUT -p tcp --dport $TCPPORT -d $HOST_IP -j ACCEPT
 done
 
-# Accept all custom UDP-ports defined by $IN_UDP_PORTS
-for UDPPORT in ${IN_UDP_PORTS[@]}; do
-  $IPTABLES -A INPUT -p udp --dport $UDPPORT -d $IP -j ACCEPT
+# Accept all custom UDP-ports defined by $PORTS_UDPIN
+for UDPPORT in ${PORTS_UDPIN[@]}; do
+  $IPTABLES -A INPUT -p udp --dport $UDPPORT -d $HOST_IP -j ACCEPT
 done
 
 # Accept incoming icmp-requests (ping)
-$IPTABLES -A INPUT -p icmp --icmp-type 8 -d $IP -j ACCEPT
+$IPTABLES -A INPUT -p icmp --icmp-type 8 -d $HOST_IP -j ACCEPT
 # Block incoming brodcast
 $IPTABLES -A INPUT -m pkttype --pkt-type broadcast -j DROP
 
 # Decide if logging unmatched INPUT traffic is to be done.
 if [ $LOGGING = true -o $(echo $LOGGING | tr [:lower:] [:upper:]) = INPUT ]; then
-  $IPTABLES -A INPUT -j $LOGINPUTCHAIN
+  $IPTABLES -A INPUT -j $CHAIN_LOGINPUT
 fi
 
 # Be polite and reject packages instead of dropping them, to a limit.
-$IPTABLES -A INPUT -p icmp $LIMIT -d $IP -j REJECT --reject-with icmp-admin-prohibited
-$IPTABLES -A INPUT -p udp $LIMIT -d $IP -j REJECT --reject-with icmp-port-unreachable
-$IPTABLES -A INPUT -p tcp $LIMIT -d $IP -j REJECT --reject-with tcp-reset
+$IPTABLES -A INPUT -p icmp $LIMIT -d $HOST_IP -j REJECT --reject-with icmp-admin-prohibited
+$IPTABLES -A INPUT -p udp $LIMIT -d $HOST_IP -j REJECT --reject-with icmp-port-unreachable
+$IPTABLES -A INPUT -p tcp $LIMIT -d $HOST_IP -j REJECT --reject-with tcp-reset
 
 
 ####################### IPTABLES OUTPUT RULES ##################################
@@ -171,34 +171,34 @@ $IPTABLES -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 $IPTABLES -A OUTPUT -o lo -j ACCEPT
 
 # Accept dns lookups (udp and tcp)
-$IPTABLES -A OUTPUT -p tcp --dport 53 -s $IP -j ACCEPT
-$IPTABLES -A OUTPUT -p udp --dport 53 -s $IP -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --dport 53 -s $HOST_IP -j ACCEPT
+$IPTABLES -A OUTPUT -p udp --dport 53 -s $HOST_IP -j ACCEPT
 
 # Accept outgoing SSH, HTTP and FTP connections.
-$IPTABLES -A OUTPUT -p tcp --dport 22 -s $IP -j ACCEPT
-$IPTABLES -A OUTPUT -p tcp --dport 20:21 -s $IP -j ACCEPT
-$IPTABLES -A OUTPUT -p tcp -m multiport --dports 80,443 -s $IP -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --dport 22 -s $HOST_IP -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --dport 20:21 -s $HOST_IP -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp -m multiport --dports 80,443 -s $HOST_IP -j ACCEPT
 
-# Accept all custom TCP-ports defined by $OUT_UDP_PORTS
-for TCPPORT in ${OUT_TCP_PORTS[@]}; do
-  $IPTABLES -A OUTPUT -p tcp --dport $TCPPORT -s $IP -j ACCEPT
+# Accept all custom TCP-ports defined by $PORTS_UDPOUT
+for TCPPORT in ${PORTS_TCPOUT[@]}; do
+  $IPTABLES -A OUTPUT -p tcp --dport $TCPPORT -s $HOST_IP -j ACCEPT
 done
 
-# Accept all custom UDP-ports defined by $OUT_UDP_PORTS
-for UDPPORT in ${OUT_UDP_PORTS[@]}; do
-  $IPTABLES -A OUTPUT -p udp --dport $UDPPORT -s $IP -j ACCEPT
+# Accept all custom UDP-ports defined by $PORTS_UDPOUT
+for UDPPORT in ${PORTS_UDPOUT[@]}; do
+  $IPTABLES -A OUTPUT -p udp --dport $UDPPORT -s $HOST_IP -j ACCEPT
 done
 
 # Accept outgoing icmp-requests
-$IPTABLES -A OUTPUT -p icmp --icmp-type 8 -s $IP -j ACCEPT
+$IPTABLES -A OUTPUT -p icmp --icmp-type 8 -s $HOST_IP -j ACCEPT
 
 # Accept outgoing broadcast messages
-$IPTABLES -A OUTPUT -d $BCAST -s $IP -j ACCEPT
-$IPTABLES -A OUTPUT -d 255.255.255.255 -s $IP -j ACCEPT
+$IPTABLES -A OUTPUT -d $HOST_BCAST -s $HOST_IP -j ACCEPT
+$IPTABLES -A OUTPUT -d 255.255.255.255 -s $HOST_IP -j ACCEPT
 
 # Decide if logging unmatched OUTPUT traffic is to be done.
 if [ $LOGGING = true -o $(echo $LOGGING | tr [:lower:] [:upper:]) = OUTPUT ]; then
-  $IPTABLES -A OUTPUT -j $LOGOUTPUTCHAIN
+  $IPTABLES -A OUTPUT -j $CHAIN_LOGOUTPUT
 fi
 
 # vim: set expandtab ts=2 sw=2:
